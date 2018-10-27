e196e48730c037da035b049a52fcb21e85280bb2
kranthi <kranthidevavarapu@gmail.com>
2018-10-27 15:29:52 -0400

    commit

---
+++ kranthiLearn/src/test/java/Samples/first.java
@@ -0,0 +1,14 @@
+package Samples;
+
+public class first {
+    public static void main(String[] args) {
+
+        int i;
+        int[] arr = {1, 2, 3};
+        for (i = 0; i <= 2; i++) {
+            System.out.println("print" + i);
+
+
+        }
+    }
+}
